88f598bd0c62fbc394da4b97b7ca5a90b99344a0
YossefAyman <55799355+YossefAyman@users.noreply.github.com>
2023-03-09 12:20:08 +0200

    Added modules test

---
+++ ERP/Modules_Test/Integration Module Test/CheckAddingPermisionWithRestrctions_Test.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using time = System.Threading.Thread;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Automation_Testing;
+using Aspose.Pdf.Operators;
+
+namespace ERP_Automation_Testing
+{
+    [TestFixture]
+    class M8_IntegrationTests_N1_CheckAddingPermisionWithREstrictions
+    {
+        [SetUp]
+        public static void Test_Init()
+        {
+            Automation_Testing.Common.Driver.Manage().Window.Maximize();
+            Login_Page.LoginAsAdmin();
+        }
+        [Test]
+        public static void T1_CheckAddingPermisionWithREstrictions()
+        {
+           
+            Daily_Restrictions.Goto();
+            string dailyRestrictionsID = Daily_Restrictions.GettingLastID();
+
+            Addingpermission_page.Goto();
+
+            int countValueBeforeAdding = Common.ReadCountText();//0
+            Addingpermission_page.Add_AddingPermission();
+            int countValueAfterAdding = Common.ReadCountText();//1
+
+            Assert.IsTrue(countValueAfterAdding - countValueBeforeAdding == 1, "T1_Add permission Failed");
+
+            Daily_Restrictions.Goto();
+            int valueDebtor, valueCredit;
+
+            Daily_Restrictions.Verifing_debtor_Creditor_Values(dailyRestrictionsID, out valueDebtor, out valueCredit);
+            int totalAmout = int.Parse(Data.AddingPermission.Price) * int.Parse(Data.AddingPermission.Quantity);
+            Assert.AreEqual(totalAmout, valueDebtor);
+
+            Common.Driver.Close();
+
+
+        }
+
+
+    }
+}
---
+++ ERP/Modules_Test/Sales Module Test/SalesBill_Test.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using time = System.Threading.Thread;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+//using System.Windows.Forms;
+
+
+
+namespace ERP_Automation_Testing
+{
+
+    class M6_Sales_N1_SalesBill_Test
+    {
+        [SetUp]
+        public static void Test_Init()
+        {
+            Automation_Testing.Common.Driver.Manage().Window.Maximize();
+            Login_Page.LoginAsAdmin();
+            SalesBill_Page.Goto();
+        }
+
+        [Test]
+        [TestCase("Cash")]
+        [TestCase("Bank_Transfer")]
+        [TestCase("Cheque")]
+        [TestCase("Payment_Cards")]
+
+        public void T1_Add_SalesBill_Test(string Test_PaymentMethod)
+        {
+            string NoOfItemsBefore = " ", NoOfItemsAfter = " ";
+            SalesBill_Page.Search(""); // BUG "عدم ظهور فواتير المشتريات الا بعد الضغط على زرار البحث"
+
+            NoOfItemsBefore = SalesBill_Page.Number_Of_Items();
+            Data.SalesBill NewSalesBill = new Data.SalesBill();
+
+            switch (Test_PaymentMethod)
+            {
+                case "Cash":
+                    NewSalesBill.PayMethod = "نقدا";
+                    break;
+                case "Bank_Transfer":
+                    NewSalesBill.PayMethod = "تحويل بنكى";
+                    break;
+                case "Cheque":
+                    NewSalesBill.PayMethod = "شيك";
+                    break;
+                case "Payment_Cards":
+                    NewSalesBill.PayMethod = "بطاقات دفع";
+                    break;
+                default:
+                    NewSalesBill.PayMethod = "نقدا";
+                    break;
+            }
+            NewSalesBill.CostCenter = "مبيعات مصباح";
+            // NewSalesBill.PayMethod = Test_PaymentMethod;
+            SalesBill_Page.Add_SalesBill(NewSalesBill);
+            NoOfItemsAfter = SalesBill_Page.Number_Of_Items();
+
+
+            Assert.True(NoOfItemsBefore != NoOfItemsAfter, "T1_Add_SalesBill_" + Test_PaymentMethod + "_Test Failed");
+
+        }
+
+    }
+}
